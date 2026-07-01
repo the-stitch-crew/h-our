1e802ca55bf85e7395bdfdcc03a6f63fc55d18b2
Admin <wlsql852@naver.com>
2026-07-01 10:35:37 +0900

    git init

---
+++ src/main/java/stitch/crew/hour/HourApplication.java
@@ -0,0 +1,13 @@
+package stitch.crew.hour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HourApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HourApplication.class, args);
+    }
+
+}
---
+++ src/test/java/stitch/crew/hour/HourApplicationTests.java
@@ -0,0 +1,13 @@
+package stitch.crew.hour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HourApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
