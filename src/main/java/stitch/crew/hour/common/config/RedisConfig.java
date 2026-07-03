656439e730cf38b57e2898342d6b71e07575f5c8
FA-50 <wjdtn747@naver.com>
2026-07-03 15:31:51 +0900

    [FEAT] Redis 관련 초기설정

---
+++ src/main/java/stitch/crew/hour/common/config/RedisConfig.java
@@ -0,0 +1,61 @@
+package stitch.crew.hour.common.config;
+
+import lombok.RequiredArgsConstructor;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import stitch.crew.hour.common.config.properties.RedisProperties;
+
+@Configuration
+@RequiredArgsConstructor
+public class RedisConfig {
+    private final RedisProperties redisProperties;
+
+    // 분산락 전용
+    @Bean
+    public RedissonClient redissonClient(){
+        Config config = new Config();
+        String uri = String.format("redis://%s:%s", redisProperties.getHost(), redisProperties.getPort());
+        config.useSingleServer().setAddress(uri);
+        return Redisson.create(config);
+    }
+
+    // Refresh Token 전용 DB
+    @Bean
+    @Primary
+    public RedisConnectionFactory authRedisConnectionFactory(){
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(
+                redisProperties.getHost(),
+                redisProperties.getPort()
+        );
+        config.setDatabase(1);
+        return new LettuceConnectionFactory(config);
+    }
+
+    // 캐시용
+    @Bean
+    public RedisConnectionFactory cacheRedisConnectionFactory() {
+        RedisStandaloneConfiguration config =
+                new RedisStandaloneConfiguration(
+                        redisProperties.getHost(),
+                        redisProperties.getPort()
+                );
+        config.setDatabase(2); // 캐시용
+        return new LettuceConnectionFactory(config);
+    }
+    @Bean
+    @Qualifier("cache")
+    public RedisTemplate<String,String> cacheRedisTemplate(){
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(cacheRedisConnectionFactory());
+        return redisTemplate;
+    }
+}
