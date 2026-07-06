b7dfa4843af85f5477754df42c5026404cdc3f05
한승우 <tmddn_00@naver.com>
2026-07-07 01:44:35 +0900

    [ADD] CurrentUser 구현체 수정

--- src/main/java/stitch/crew/hour/user/domain/CurrentUser.java
+++ src/main/java/stitch/crew/hour/user/domain/CurrentUser.java
@@ -1,6 +1,13 @@
 package stitch.crew.hour.user.domain;
 
 import java.security.Principal;
+import java.util.Collection;
+import java.util.List;
+
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -8,7 +15,7 @@ import stitch.crew.hour.user.constant.Role;
 
 @Getter
 @RequiredArgsConstructor
-public class CurrentUser implements Principal {
+public class CurrentUser implements UserDetails {
 
 	private final Long id;
 	private final String email;
@@ -23,7 +30,19 @@ public class CurrentUser implements Principal {
 	}
 
 	@Override
-	public String getName() {
-		return email;
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return List.of(
+			new SimpleGrantedAuthority(role.getValue())
+		);
+	}
+
+	@Override
+	public @Nullable String getPassword() {
+		return "";
+	}
+
+	@Override
+	public String getUsername() {
+		return "";
 	}
 }
