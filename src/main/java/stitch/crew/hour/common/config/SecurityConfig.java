dfe4f8067bee3845b2609b5ede72427b568fbd4a
한승우 <tmddn_00@naver.com>
2026-07-07 16:58:20 +0900

    [Feature] 필터체인에서 실패 응답 템플릿 잡아주기

--- src/main/java/stitch/crew/hour/common/config/SecurityConfig.java
+++ src/main/java/stitch/crew/hour/common/config/SecurityConfig.java
@@ -1,18 +1,23 @@
 package stitch.crew.hour.common.config;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpMethod;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
+import stitch.crew.hour.common.config.entrypoint.JwtAccessDeniedHandler;
+import stitch.crew.hour.common.config.entrypoint.JwtAuthenticationEntryPoint;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -21,6 +26,10 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .httpBasic(basic -> basic.disable())
                 .formLogin(formLogin -> formLogin.disable())
+                .exceptionHandling(exp -> exp
+                    .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                    .accessDeniedHandler(jwtAccessDeniedHandler)
+                )
                 .authorizeHttpRequests(auth ->auth
                     .requestMatchers(HttpMethod.POST, "/api/users/signup").permitAll()
                     .requestMatchers("/api/auth/**").permitAll()
